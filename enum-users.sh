b8ec4bd4f79205df6ce0e316c7b251c7bc2086e7
zer0da <65029938+zer0da@users.noreply.github.com>
2022-01-31 15:34:24 +0300

    Update enum-users.sh
    last

--- enum-users.sh
+++ enum-users.sh
@@ -5,6 +5,9 @@ echo "This tool is doing your command for 0/24(0-255) ranges for the ip block yo
 read -p "ip block for the users (ex: 192.168.0.0/24):  " ip
 read -p "give an outputfile for the usernames: " output
 
-nmap -p 139,389,445 --script smb-enum-users.nse -oN /tmp/nmap-smb-users.txt ${ip}
+nmap -p 139,389,445 --script smb-enum-users.nse -oN /tmp/nmap-smb-users.txt ${ip} >> /dev/null 2>&1
 
 cat /tmp/nmap-smb-users.txt | grep 'Nmap scan report for' | grep '(' | cut -f '5' -d " " | cut -f '1' -d "." > ${output}
+
+echo " "
+cat ${output}
